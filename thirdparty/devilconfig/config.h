51c50a652b232405773a3bd7e1f2d649fb300aed
ficool2 <34815548+ficool2@users.noreply.github.com>
2026-08-17 15:12:54 +0100

    Submodule gitlinks

---
+++ thirdparty/devilconfig/config.h
@@ -0,0 +1,28 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// handwritten config
+
+/* formats we aren't supporting */
+#define IL_NO_EXR    // OpenEXR
+#define IL_NO_JP2    // JasPer
+#define IL_NO_LCMS   // Little CMS
+#define IL_NO_MNG    // libmng
+#define IL_NO_TIF    // libtiff
+#define IL_NO_WDP    // Microsoft HD Photo
+
+/* built from source */
+/* #undef IL_NO_PNG */
+/* #undef IL_NO_JPG */
+
+/* DevIL handle this */
+/* #undef IL_USE_DXTC_NVIDIA */
+/* #undef IL_USE_DXTC_SQUISH */
+
+/* linked through CMake */
+/* #undef IL_USE_PRAGMA_LIBS */
+
+/* little endian */
+/* #undef WORDS_BIGENDIAN */
+
+#endif //__CONFIG_H__
