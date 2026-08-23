70167d7e0eaafd7537905787da376b373f213d2f
ficool2 <34815548+ficool2@users.noreply.github.com>
2026-08-23 13:23:55 +0100

    Bump VTFLib version to 2.2.2

--- VTFLib/VTFLib.h
+++ VTFLib/VTFLib.h
@@ -139,8 +139,8 @@ namespace VTFLib
 	extern vlUInt uiVMTParseMode;
 }
 
-#define VL_VERSION			221			//!< VTFLib version as integer
-#define VL_VERSION_STRING	"2.2.1"		//!< VTFLib version as string
+#define VL_VERSION			222			//!< VTFLib version as integer
+#define VL_VERSION_STRING	"2.2.2"		//!< VTFLib version as string
 
 #ifdef __cplusplus
 extern "C" {
